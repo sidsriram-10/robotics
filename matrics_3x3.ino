d54949f8b009ad4d12bdab0827bdbe6f960f7799
sidsriram-10 <siddharth.sriram.work@gmail.com>
2025-05-13 20:30:25 -0700

    Create matrics_3x3.ino

---
+++ matrics_3x3.ino
@@ -0,0 +1,83 @@
+// C++ code
+//
+void setup()
+{pinMode(2,OUTPUT);
+ pinMode(3,OUTPUT);
+ pinMode(4,OUTPUT);
+ pinMode(7,OUTPUT);
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+ 
+ digitalWrite(7,LOW);
+ digitalWrite(8,LOW);
+ digitalWrite(9,LOW);
+}
+
+void loop()
+{
+  digitalWrite(2,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(2,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  delay(500);
+}
+ 
+ 
